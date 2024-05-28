4e985ff34e296a16d0ceaccbba6bb2faf0d10a4d
rustam0927 <rustamrahmonjonov89@gmail.com>
2024-05-28 11:52:36 +0500

    commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import { create } from 'express-handlebars'
+import mongoose from 'mongoose';
+import session from 'express-session';
+import flash from 'connect-flash'
+import cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv'
+
+import varMiddleware from './middleware/var.js';
+import userMiddleware from './middleware/user.js';
+import hbsHelper from './utils/index.js'
+
+import AuthRoutes from './routes/auth.js'
+import ProductsRoutes from './routes/products.js'
+
+
+dotenv.config()
+
+const app = express();
+
+const hbs = create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+    helpers: hbsHelper,
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(session({secret: "Rustam", resave: false, saveUninitialized: false}))
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
+
+
+app.use(AuthRoutes)
+app.use(ProductsRoutes)
+
+const startApp = () => {
+    try {
+        
+        mongoose.set('strictQuery', false)
+        mongoose.connect(process.env.MONGO_URI,
+            {
+                useNewUrlParser: true,
+            },
+            () => console.log('Mongo DB connected')
+        )
+        
+        const PORT = process.env.PORT || 4100
+        app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`) )
+    } catch (error) {
+        console.log(error)
+    }
+}
+startApp()
+
